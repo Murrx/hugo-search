67428e1e7b7bc2a91454da56952fe7b82ca032c2
Murrx <altenarobin@gmail.com>
2021-01-26 13:46:55 +0100

    define the html structure by insterting a template tag

--- assets/hugoSearch/components/SearchResults.ts
+++ assets/hugoSearch/components/SearchResults.ts
@@ -1,32 +1,46 @@
 import { requestResultsStore } from "../eventHelper";
 import { QueryData } from "../types/SearchStore.type";
-import ResultCard from "./ResultCard";
 
 export default class SearchResults extends HTMLElement {
-  private _template = document.createElement("template");
+  private _templateElement = document.createElement("template");
+  private template: { parts: string[]; args: RegExpMatchArray } = {
+    parts: [],
+    args: [],
+  };
 
   constructor() {
     super();
   }
+
   connectedCallback() {
+    let template = this.getElementsByTagName("template")[0];
+    this.parseTemplate(template.innerHTML);
     let resultsStore = requestResultsStore(this);
     resultsStore.subscribe(this.onResultsChange);
-    this.appendChild(this._template.content.cloneNode(true));
+    this.appendChild(this._templateElement.content.cloneNode(true));
   }
 
   onResultsChange = (data: QueryData[]) => {
-    while (this.firstChild) {
-      this.removeChild(this.firstChild);
-    }
-    data &&
-      data.forEach((result: QueryData) => {
-        let card = new ResultCard({
-          name: result.name,
-          category: result.category,
-          sub_category: result.sub_category,
-          uri: result.uri,
-        });
-        this.appendChild(card);
-      });
+    this.render(data);
   };
+
+  render(results: QueryData[]) {
+    this._templateElement.innerHTML = results
+      .map(function (result) {
+        return this.renderFromTemplate(result);
+      }, this)
+      .join("");
+  }
+
+  parseTemplate(str: string) {
+    this.template.parts = str.split(/\$\{(?!\d)[\wæøåÆØÅ]*\}/);
+    this.template.args = str.match(/[^{\}]+(?=})/g) || [];
+  }
+
+  renderFromTemplate(result: QueryData) {
+    let parameters = this.template.args.map((argument) =>
+      result[argument] === undefined ? "" : result[argument]
+    );
+    return String.raw({ raw: this.template.parts }, ...parameters);
+  }
 }
--- assets/hugoSearch/main.ts
+++ assets/hugoSearch/main.ts
@@ -1,10 +1,7 @@
 import SearchResults from "./components/SearchResults";
-import ResultCard from "./components/ResultCard";
 import HugoSearchInput from "./components/InputField";
 import MessageBar from "./components/MessageBar";
 
-// todo: add hs-prefix
-window.customElements.define("result-card", ResultCard);
 window.customElements.define("hs-input", HugoSearchInput);
 window.customElements.define("hs-message", MessageBar);
 window.customElements.define("hs-results", SearchResults);
